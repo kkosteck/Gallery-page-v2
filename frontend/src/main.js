bf5ae3aff99a4aa986c8aabc40f4338fcf28d55b
Kacper Kostecki <01143577@pw.edu.pl>
2022-09-10 20:24:20 +0200

    added firebase hosting

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,15 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import i18n from './i18n'
 import router from './router'
 import store from './store'
 import axios from 'axios'
 // fontAwesome
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-import i18n from './i18n'
-
+document.title = "Maksiak"
 axios.defaults.baseURL = "http://127.0.0.1:8000"
-createApp(App).use(i18n)
+createApp(App)
+    .use(i18n)
     .use(store)
     .use(router)
     .component('font-awesome-icon', FontAwesomeIcon)
